8a35cb25889b60c0892c10d2b9867129e0fd6f66
MOHAMED <mohamedelafandy593@gmail.com>
2024-04-18 18:23:58 +0000

    add

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of floats in the input list."""
+    return sum(input_list)
+
