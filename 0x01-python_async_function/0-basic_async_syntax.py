a1891c4c1f9d2b79f52ef4f43a2787ab6ee0069d
MOHAMED <mohamedelafandy593@gmail.com>
2024-04-22 17:23:13 +0000

    awit

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""task 0"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float :
+"""wait for a random delay"""
+
+randomDaley = random.random() * max_delay
+awit asyncio.sleep(randomDaley)
+return randomDaley
