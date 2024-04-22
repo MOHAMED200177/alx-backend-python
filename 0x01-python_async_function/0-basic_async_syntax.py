e8099995dc090a967c66ecefdea9341be16c5d5c
MOHAMED <mohamedelafandy593@gmail.com>
2024-04-22 17:37:20 +0000

    pay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """waits for a random delay"""
-    radmonDaley = random.random() * max_delay
-    await asyncio.sleep(radmonDaley)
-    return radmonDaley
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
